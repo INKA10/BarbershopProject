c4f489d2f54545a2daa28321d8a6d42145e1586c
Andy Suarez <andysuarez36@gmail.com>
2020-01-21 12:20:29 -0800

    changes from Jeff

--- routes.js
+++ routes.js
@@ -1,13 +1,13 @@
 module.exports = function(app){
 
-	const application = require('./routes/application');
-	const users = require('./routes/users');
-	const pricing = require('./routes/pricing');
-	const appointments = require('./routes/appointments');
+		const application = require('./routes/application');
+		const users = require('./routes/users');
+		const pricing = require('./routes/pricing');
+		const appointments = require('./routes/appointments');
 
-	app.use('/', application);
-	app.use('/users', users);
-	app.use('/pricing', pricing);
-	app.use('/appointments', appointments);
-//other routes..
+		app.use('/', application);
+		app.use('/users', users);
+		app.use('/pricing', pricing);
+		app.use('/appointments', appointments);
+    //other routes..
 }
