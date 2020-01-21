c3b8d0e55a6cb9dac9dcbc553b8d2de689976d47
Andy Suarez <andysuarez36@gmail.com>
2020-01-21 11:58:54 -0800

    making deletaions on files

--- routes.js
+++ routes.js
@@ -1,15 +1,13 @@
 module.exports = function(app){
 
-		const application = require('./routes/application');
-		const users = require('./routes/users');
-		const trips = require('./routes/trips');
-		const pricing = require('./routes/pricing');
-		const appointments = require('./routes/appointments');
+	const application = require('./routes/application');
+	const users = require('./routes/users');
+	const pricing = require('./routes/pricing');
+	const appointments = require('./routes/appointments');
 
-		app.use('/', application);
-		app.use('/users', users);
-		app.use('/trips', trips);
-		app.use('/pricing', pricing);
-		app.use('/appointments', appointments);
-    //other routes..
+	app.use('/', application);
+	app.use('/users', users);
+	app.use('/pricing', pricing);
+	app.use('/appointments', appointments);
+//other routes..
 }
