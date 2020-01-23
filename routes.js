33ded878bca91a24dce8d56e76d618bd0983c800
Andy Suarez <andysuarez36@gmail.com>
2020-01-22 22:13:29 -0800

    adding changes from forgot password

--- routes.js
+++ routes.js
@@ -4,10 +4,12 @@ module.exports = function(app){
 		const users = require('./routes/users');
 		const pricing = require('./routes/pricing');
 		const appointments = require('./routes/appointments');
+		const password = require('./routes/password');
 
 		app.use('/', application);
 		app.use('/users', users);
 		app.use('/pricing', pricing);
 		app.use('/appointments', appointments);
+		app.use('/password', password);
     //other routes..
 }
