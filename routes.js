68fa05acc85e5efc9616da1b89faaab2df0a8a0f
Andy Suarez <andysuarez36@gmail.com>
2020-01-24 21:14:21 -0800

    adding the customer appointment portal

--- routes.js
+++ routes.js
@@ -5,13 +5,12 @@ module.exports = function(app){
 		const pricing = require('./routes/pricing');
 		const appointments = require('./routes/appointments');
 		const password = require('./routes/password');
-		const appointmentsPortal = require('./routes/appointmentsPortal');
 
 		app.use('/', application);
 		app.use('/users', users);
 		app.use('/pricing', pricing);
 		app.use('/appointments', appointments);
 		app.use('/password', password);
-		app.use('/appointmentsPortal', appointmentsPortal);
+	
     //other routes..
 }
